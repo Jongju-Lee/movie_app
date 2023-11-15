3db7f9eace382a860dba4a50eeb5a25974a34f00
jongju-Lee <jeje7@naver.com>
2023-11-16 01:31:35 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
+
+// The Movie DB API 요청을 위한 Axios 인스턴스 생성 및 요청 보내기
